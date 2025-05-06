100f581afa1e8743edb4ef9aaff8330491bac50c
Siddharth Sharma <S.S.Sharma-2@student.tudelft.nl>
2025-05-06 17:59:01 +0200

    fixing setup

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="lib-version",
     version="0.1.0",
-    packages=find_packages(),
-    install_requires=[],
+    packages=find_packages(where="versionutil"),
+    package_dir={"":"versionutil"},
+    python_requires="==3.12.9"
 )
 
