be6d75d2d24df0bd34d59e4b18e7992f3c8d80f1
Victor Hornet <victorhornet@tudelft.nl>
2025-05-06 18:33:38 +0200

    Add package_dir

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup
 
+
 setup(
     name="lib-version",
+    url="https://github.com/remla2025-team19/lib-version",
     version="0.1.0",
     packages=["lib_version"],
+    package_dir={"": "lib_version"},
     python_requires="==3.12.9",
 )
