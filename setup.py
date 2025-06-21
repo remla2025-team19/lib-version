22d0b208873317d016d8db895659d16e853488fe
Siddharth Sharma <S.S.Sharma-2@tudelft.nl>
2025-06-21 09:35:19 +0200

    Update setup.py to fix workflow (#19)

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import find_packages, setup
+import os
 
-
+# Read version from __init__.py
+def get_version():
+    init_path = os.path.join(os.path.dirname(__file__), 'src', 'lib_version', '__init__.py')
+    with open(init_path, 'r') as f:
+        for line in f:
+            if line.startswith('__version__'):
+                return line.split('=')[1].strip().strip('"').strip("'")
+    raise RuntimeError("Unable to find version string.")
 setup(
     name="lib-version",
-    version="0.1.0",
+    version=get_version(),
     author="Team 19",
     description="A lightweight utility library that provides version information for your application components.",
     url="https://github.com/remla2025-team19/lib-version",
